2da0d7d5deea3eed27a49b198fa8637aa2669309
David <davidjhinku@gmail.com>
2020-12-16 17:30:57 -0500

    Completed for the day

--- js-exercises/phase_4_recursion.js
+++ js-exercises/phase_4_recursion.js
@@ -56,6 +56,64 @@ function fibonacci(n){
 // console.log(fibonacci(8));
 
 
-function deepDup(arr)
+function deepDup(arr) {
+    if (arr.some((x) => { !x.isArray })) return [...arr];
+    return arr.map((sub) => {
+        if (sub.isArray) {
+            return deepDup(sub)
+        } else {
+            return sub
+        }
+    })
+
+    // debugger
+    // return dupped
+}
+
+// console.log(deepDup([[1, 2], [[3, 4, [5]], 6], [7, 8]]))
+
+function bsearch(arr, target, left = 0, right = arr.length - 1) {
+    // console.log(left)
+    // console.log(right)
     
-end
+    if (left >= right) {
+        if (arr[left] === target) return left
+        return -1
+    }
+    let middle = Math.floor(left + ((right - left)/ 2))
+    if (arr[middle] === target) {
+        return middle
+    } else if (arr[middle] > target) {
+        return bsearch(arr, target, left, middle - 1)
+    } else {
+        return bsearch(arr, target, middle + 1, right)
+    }
+}
+
+// console.log(bsearch([1, 2, 3, 5, 7, 9, 10, 15], 4))
+
+function mergesort(arr) {
+    if (arr.length <= 1) return arr
+
+    let mid = Math.floor(arr.length / 2)
+    let left = mergesort(arr.slice(0, mid))
+    let right = mergesort(arr.slice(mid, arr.length))
+
+    return merge(left, right)
+}
+
+function merge(left, right) {
+    let sorted = []
+
+    if (left.empty || right.empty) {
+        if (left[0] > right[0]) {
+            sorted.push(right.shift)
+        } else {
+            sorted.push(left.shift)
+        }
+    }
+
+    return sorted.concat(left, right)
+}
+
+console.log(mergesort([3, 6, 78, 3, 5, 9, 15]))
