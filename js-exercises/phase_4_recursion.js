52967ab8896d3d418015f53ff48ebb291ff8c631
Jose Miralles <josemiralles1991@gmail.com>
2020-12-16 16:41:30 -0500

    Starting deep dup

--- js-exercises/phase_4_recursion.js
+++ js-exercises/phase_4_recursion.js
@@ -24,5 +24,38 @@ function sumRec(arr) {
     
     return result
 }
+// console.log(sumRec([2,4,6,3]));
 
-console.log(sumRec([2,4,6,3]));
+function exopnent(base, exp){
+    if (exp === 1) return base;
+
+    return exopnent( base, exp - 1 ) * base;
+}
+
+function exopnent2(base, exp){
+    if (exp === 1) return base;
+
+    if (exp % 2 === 0){
+        return exopnent2(base, exp/2) ** 2;
+    } else {
+        return base * (exopnent2(base, (exp - 1) / 2) ** 2)
+    }
+}
+// console.log(exopnent2(5, 10));
+
+
+function fibonacci(n){
+    if (n === 1) return [1];
+    if (n === 2) return [1, 1];
+
+    const previous = fibonacci(n - 1);
+    const newNum = previous[previous.length - 1] + previous[previous.length - 2];
+
+    return previous.concat(newNum);
+}
+// console.log(fibonacci(8));
+
+
+function deepDup(arr)
+    
+end
